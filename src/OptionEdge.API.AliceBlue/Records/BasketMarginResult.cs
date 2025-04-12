2bf77c95bf9766274b1c3d71e7233a56fcf5455d
optionedge <izappworld@gmail.com>
2025-04-12 23:30:32 +0530

    Margin result refactor

--- src/OptionEdge.API.AliceBlue/Records/BasketMarginResult.cs
+++ src/OptionEdge.API.AliceBlue/Records/BasketMarginResult.cs
@@ -7,10 +7,16 @@ namespace OptionEdge.API.AliceBlue.Records
 {
     public class BasketMarginResult : BaseResponseResult
     {
+        [JsonProperty("result")]
+        public MarginResultData Result { get; set; }
+    }
+
+    public class MarginResultData
+    {
         [JsonProperty("marginUsed")]
         public decimal MarginUsed { get; set; }
 
         [JsonProperty("marginUsedTrade")]
-        public decimal MarginUsedTrade { get; set; }
+        public Decimal MarginUsedTrade { get; set; }
     }
 }
